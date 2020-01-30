bf857793c919e9b3854aeae518c85d5aaf5ee615
Ruiner85 <paul.m.mackay85@gmail.com>
2020-01-30 14:38:43 +0000

    Creation of Classes

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -1,3 +1,18 @@
+require_relative("..db/sql_runner")
+
 class Movie
+
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @title=options['title']
+  @genre=options['genre']
+end
+
+
+
+
+
+
+
 
 end
